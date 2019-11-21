088deaefac3ffd26be026692f05336b9c77af529
Hideki Abe <hideki.abe.0630@gmail.com>
2019-11-21 21:22:14 +0900

    Add L10N

---
+++ plugins/CDRelatedSource/lib/CDRelatedSource/L10N/en_us.pm
@@ -0,0 +1,12 @@
+package CDRelatedSource::L10N::en_us;
+
+use strict;
+use base 'CDRelatedSource::L10N';
+use vars qw( %Lexicon );
+
+%Lexicon = (
+    '_PLUGIN_DESCRIPTION' => 'Provides a tag to get the ID of the content data that refers to itself in some content data.',
+    '_PLUGIN_AUTHOR' => 'Hideki Abe'
+);
+
+1;
---
+++ plugins/CDRelatedSource/lib/CDRelatedSource/L10N/ja.pm
@@ -0,0 +1,13 @@
+package CDRelatedSource::L10N::ja;
+
+use strict;
+use base 'CDRelatedSource::L10N::en_us';
+use vars qw( %Lexicon );
+
+%Lexicon = (
+    'Contents Data Related Source Plugin' => 'リンク元コンテンツデータ取得プラグイン',
+    '_PLUGIN_DESCRIPTION' => 'あるコンテンツデータにおいて自身を参照しているコンテンツデータのIDを取得するタグを提供します。',
+    '_PLUGIN_AUTHOR' => '安倍 英樹'
+);
+
+1;
